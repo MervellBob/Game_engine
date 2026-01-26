00f4655d6b86cfe0b68879f9a168dbb20e2400b5
Mervell Bob <mervell.bob@proton.me>
2026-01-26 10:47:07 +0100

    Refacto: Commented out old code, waiting confirmation for deletion

--- crates/utils/src/sld.rs
+++ crates/utils/src/sld.rs
@@ -1,45 +1,45 @@
-use sdl3::*;
-use events::sdl::get_event;
+// use sdl3::*;
+// use events::sdl::get_event;
 
-pub struct Context {
-    sdl: Sdl,
-    video: VideoSubsystem,
-    canva: Canvas,
-    event_pump: EventPump,
-}
+// pub struct Context {
+//     sdl: Sdl,
+//     video: VideoSubsystem,
+//     canva: Canvas,
+//     event_pump: EventPump,
+// }
 
-impl Context {
-    pub fn new() -> Self {
-        let sdl_context = sdl3::init().unwrap();
-        let video_subsystem = sdl_context.video().unwrap();
-        video_subsystem.window("Test", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
-        let mut canva = window.into_canvas();
-        let event_pump = sdl_context.event_pump().unwrap();
+// impl Context {
+//     pub fn new() -> Self {
+//         let sdl_context = sdl3::init().unwrap();
+//         let video_subsystem = sdl_context.video().unwrap();
+//         video_subsystem.window("Test", 800, 600)
+//         .position_centered()
+//         .build()
+//         .unwrap();
+//         let mut canva = window.into_canvas();
+//         let event_pump = sdl_context.event_pump().unwrap();
 
-        Context {
-            sdl: sdl_context,
-            video: video_subsystem,
-            canva: canva,
-            event_pump: event_pump,
-        }
-    }
+//         Context {
+//             sdl: sdl_context,
+//             video: video_subsystem,
+//             canva: canva,
+//             event_pump: event_pump,
+//         }
+//     }
 
-    pub fn clear(&mut self) {
-        self.canva.clear();
-    }
+//     pub fn clear(&mut self) {
+//         self.canva.clear();
+//     }
 
-    pub fn update(&mut self) {
-        self.canva.present();
-    }
-}
+//     pub fn update(&mut self) {
+//         self.canva.present();
+//     }
+// }
 
-impl Iterator for utils::sdl3::Context {
-    type Item = events::Event;
+// impl Iterator for utils::sdl3::Context {
+//     type Item = events::Event;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        get_event(self.event_pump.poll_event())
-    }
-}
+//     fn next(&mut self) -> Option<Self::Item> {
+//         get_event(self.event_pump.poll_event())
+//     }
+// }
