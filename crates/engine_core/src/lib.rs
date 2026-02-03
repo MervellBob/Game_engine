ef3051aac49b6fb02cd6b11fe9e878105b917b3a
Mervell Bob <mervell.bob@proton.me>
2026-02-03 11:50:43 +0100

    Feat: Implemented tempo draw functions into engine core.
    -Enables the uses of shaders in the main window

--- crates/engine_core/src/lib.rs
+++ crates/engine_core/src/lib.rs
@@ -3,6 +3,7 @@ use window::window_handler::window_handler::WindowHandler;
 use events::sdl_binding::event_manager::EventManager;
 use events::sdl_binding::window_events::WindowEvent;
 use std::time::Duration;
+use rendering::shaders::shaders_utility::Shader;
 
 
 /// Entry point of the engine core.
@@ -21,7 +22,7 @@ pub fn start_engine() {
 /// Creates the main game window using the window handler.
 fn create_app_window() -> WindowHandler {
     let window_name = "We need a name for the game engine".to_string();
-    let window_with: u32 = 500;
+    let window_with: u32 = 600;
     let window_height: u32 = 600;
 
     let window_handler = WindowHandler::new(window_name, window_with, window_height);
@@ -43,6 +44,9 @@ fn start_game_loop(mut window_handler:WindowHandler) {
     let _enter = game_loop_span.enter();
     info!("Starting game loop");
 
+    let shader_program = Shader::new();
+    let vao = tempo_prepare_triangle();
+
     let mut event_manager:EventManager = EventManager::new();
     let mut game_running = true;
     while game_running {
@@ -51,7 +55,9 @@ fn start_game_loop(mut window_handler:WindowHandler) {
             event_manager.match_event_types(event_pump);
         }
         game_running = !check_closed_requested(& mut event_manager);
-        window_handler.canvas().present();
+
+        tempo_draw_triangle(&mut window_handler,&shader_program,vao);
+
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
         // Sleep is not accurate in timing it has a 1ms to 15 ms overshoot apparently, and 1/20th of a sec is ~50 ms 
     }
@@ -76,4 +82,72 @@ fn check_closed_requested(event_manager: & mut EventManager) -> bool {
         }
     }
     return  false;
+}
+
+
+
+
+type Vertex = [f32; 3];
+const VERTICES: [Vertex; 6] = [
+    [ 0.5, -0.5, 0.0], // position 0
+    [ 1.0,  0.0, 0.0], // couleur 0
+    [-0.5, -0.5, 0.0], // position 1
+    [ 0.0,  1.0, 0.0], // couleur 1
+    [ 0.0,  0.5, 0.0], // position 2
+    [ 0.0,  0.0, 1.0], // couleur 2
+];
+
+fn tempo_draw_triangle(window_handler:&mut WindowHandler, shader_program:&Shader, vao:u32) {
+    unsafe {
+        gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+        gl::Clear(gl::COLOR_BUFFER_BIT);
+
+        shader_program.use_program();
+        gl::BindVertexArray(vao);
+        gl::DrawArrays(gl::TRIANGLES, 0, 3);
+
+        window_handler.window().gl_swap_window();
+    }
+}
+
+fn tempo_prepare_triangle() -> u32{
+    let mut vbo = 0;
+    unsafe { 
+        gl::GenBuffers(1,&mut vbo);
+        assert_ne!(vbo,0);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER,vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            size_of_val(&VERTICES) as isize,
+            VERTICES.as_ptr().cast(),
+            gl::STATIC_DRAW,
+        );
+
+        let mut vao = 0; //Vertex array object
+        gl::GenVertexArrays(1,&mut vao);
+        assert_ne!(vao,0);
+        
+        gl::BindVertexArray(vao);
+
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            (2*size_of::<Vertex>()).try_into().unwrap(),
+            0 as *const _
+        );
+        gl::EnableVertexAttribArray(0); 
+                
+        gl::VertexAttribPointer(1,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            (2*size_of::<Vertex>()).try_into().unwrap(),
+            12 as *const _
+        );
+        gl::EnableVertexAttribArray(1);  
+        vao
+    }
 }
